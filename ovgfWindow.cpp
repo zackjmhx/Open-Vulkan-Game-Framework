6267e73aff8fab78f95f7ea241341684daf47d26
zackjmhx <zack.jmh@gmail.com>
2018-01-30 12:18:56 -0500

    Encapsulate most of the window functionality

--- ovgfWindow.cpp
+++ ovgfWindow.cpp
@@ -3,8 +3,21 @@
 
 
 class OvgfWindow {
-	
+
 public:
+
+
+
+private:
+
+	OvgfWindow(int width, int height) {
+		initWindow(width, height);
+	}
+
+	GLFWwindow *window = nullptr;
+	int width;
+	int height;
+
 	void initWindow(int width, int height) {
 
 		if (!glfwInit())
@@ -12,6 +25,7 @@ public:
 
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); //turn off resizeability for now
 
 		window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
 
@@ -26,7 +40,7 @@ public:
 		/*
 		TriangleBasicsApp *app = reinterpret_cast<TriangleBasicsApp *>(glfwGetWindowUserPointer(window));
 		app->recreateSwapchain();
-		
+
 		Need to determine who should properly own the swap chain before implmenting this. Eventually the main purpose of this function will be overloading for custom resize code.
 
 		It might just be easiest to disallow window resizing. Most games do.
@@ -37,10 +51,6 @@ public:
 	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 	{
 		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-			glfwSetWindowShouldClose(window, GLFW_TRUE); 
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
-
-
-private:
-	GLFWwindow *window;
 };
