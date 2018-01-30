7ccf76cda31b2a41611b6a39713e1cdc03a9df10
zackjmhx <zack.jmh@gmail.com>
2018-01-30 14:05:46 -0500

    Fix Java style code

--- ovgfGame.cpp
+++ ovgfGame.cpp
@@ -1,19 +1,35 @@
 /**OVGF is meant, for now, to be a Vulkan based rapid prototyping tool to assist in game development.
 ***Once the framework is sufficient as such a tool, then development will shift focus to being suitable for release products*/
 #include "ovfgGame.h"
+#include <chrono>
+#include <iostream>
 
-class OvgfGame {
+void OvgfGame::launch() {
 
-public:
+	if (window.getOvgfWindow(600, 400) == nullptr)
+		throw std::runtime_error("Failed to create OVGF window.");
 
-	void launch() {
+	mainLoop();
+}
 
-		if (window.getOvgfWindow(600, 400) == nullptr)
-			throw std::runtime_error("Failed to create OVGF window.");
+
+
+void OvgfGame::mainLoop() {
+
+	auto times = std::chrono::high_resolution_clock::now();
+	uint32_t frames = 0;
+
+	while (!glfwWindowShouldClose(window.getOvgfWindow())) {
+
+		glfwPollEvents();
+
+		frames++;
+
+		if (std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - times).count() >= 1) {
+			std::cout << "Current FPS: " << frames << std::endl;
+			frames = 0;
+			times = std::chrono::high_resolution_clock::now();
+		}
 	}
 
-private:
-	OvgfWindow window; //Internally relies on GLFW for window abstraction - implements basic functionality
-
-
-};
+}
