a982adbd6ef15f0b266b1e34feb4fbf8aeb7ddce
zackjmhx <zack.jmh@gmail.com>
2018-01-30 12:49:10 -0500

    Rename Game class files to match framework naming convention

--- ovgfGame.cpp
+++ ovgfGame.cpp
@@ -1,6 +1,6 @@
 /**OVGF is meant, for now, to be a Vulkan based rapid prototyping tool to assist in game development.
 ***Once the framework is sufficient as such a tool, then development will shift focus to being suitable for release products*/
-#include "Game.h"
+#include "ovfgGame.h"
 
 class OvgfGame {
 
