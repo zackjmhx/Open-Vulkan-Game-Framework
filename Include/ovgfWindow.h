4e4bdfed17fabf088f1b6654d1af40bdc3812007
zackjmhx <zack.jmh@gmail.com>
2018-01-30 14:00:03 -0500

    Add proper OvgfWindow forward call

--- Include/ovgfWindow.h
+++ Include/ovgfWindow.h
@@ -7,5 +7,24 @@
 
 class OvgfWindow {
 public:
-	GLFWwindow getOvgfWindow(int width, int height);
-};#endif
+	GLFWwindow* getOvgfWindow(int width, int height);
+	GLFWwindow* OvgfWindow::getOvgfWindow();
+
+private:
+	OvgfWindow(int width, int height);
+
+	OvgfWindow();
+
+	
+
+	void initWindow(int width, int height);
+
+	static void onWindowResize(GLFWwindow *window, int width, int height);
+
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+	GLFWwindow *window = nullptr;
+	int width;
+	int height;
+};
+#endif
