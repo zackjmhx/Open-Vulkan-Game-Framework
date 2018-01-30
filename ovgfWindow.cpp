a8347f4a49cf098ffef580cab8fd8c4cac31b031
zackjmhx <zack.jmh@gmail.com>
2018-01-30 11:35:00 -0500

    Add window abstraction class with basic callback functionality

---
+++ ovgfWindow.cpp
@@ -0,0 +1,46 @@
+/* Function definitions for current GLFW options*/
+#include "ovgfWindow.h"
+
+
+class OvgfWindow {
+	
+public:
+	void initWindow(int width, int height) {
+
+		if (!glfwInit())
+			throw std::runtime_error("Failed to init GLFW");
+
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+		window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+
+		//glfwSetWindowUserPointer(window, this);
+		glfwSetWindowSizeCallback(window, onWindowResize);
+
+	}
+
+	static void onWindowResize(GLFWwindow *window, int width, int height) {
+		if (width == 0 || height == 0) return;
+
+		/*
+		TriangleBasicsApp *app = reinterpret_cast<TriangleBasicsApp *>(glfwGetWindowUserPointer(window));
+		app->recreateSwapchain();
+		
+		Need to determine who should properly own the swap chain before implmenting this. Eventually the main purpose of this function will be overloading for custom resize code.
+
+		It might just be easiest to disallow window resizing. Most games do.
+		*/
+
+	}
+
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GLFW_TRUE); 
+	}
+
+
+private:
+	GLFWwindow *window;
+};
---
+++ ovgfWindow.h
@@ -0,0 +1,5 @@
+/* For now we're relying on GLFW for window abstraction.*/
+
+#pragma once
+#define GLFW_INCLUDE_VULKAN //Turn on glfw vulkan support
+#include <GLFW\glfw3.h>
