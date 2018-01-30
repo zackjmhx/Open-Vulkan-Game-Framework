0ba2efc6afe8c3e828823b3a4141790b7bc73a2a
zackjmhx <zack.jmh@gmail.com>
2018-01-30 12:17:54 -0500

    Include forward calls to class

--- ovgfWindow.h
+++ ovgfWindow.h
@@ -4,3 +4,8 @@
 #define GLFW_INCLUDE_VULKAN //Turn on glfw vulkan support
 #include <GLFW\glfw3.h>
 #include <stdexcept>
+
+class OvgfWindow {
+public:
+	GLFWwindow getOvgfWindow(int width, int height);
+};
