3e378468cf840b2ceda47c428f4193c64d96964b
zackjmhx <zack.jmh@gmail.com>
2018-01-30 14:06:03 -0500

    Update class definition

--- Include/ovfgGame.h
+++ Include/ovfgGame.h
@@ -6,5 +6,9 @@
 class OvgfGame {
 public:
 	void launch();
+
+private:
+	OvgfWindow window; //Internally relies on GLFW for window abstraction - implements basic functionality
+	void mainLoop();
 };
 #endif
