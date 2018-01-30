eabf5de2ffad714d01bf493f88adc1aef796b6a4
zackjmhx <zack.jmh@gmail.com>
2018-01-30 11:08:34 -0500

    Initial Push

---
+++ Game.cpp
@@ -0,0 +1,2 @@
+/**OVGF is meant, for now, to be a Vulkan based rapid prototyping tool to assist in game development.
+***Once the framework is sufficient as such a tool, then development will shift focus to being suitable for release products*/
