90ad64bdd4bc6770ab09c08f0dc8f9d532201ffb
zackjmhx <zack.jmh@gmail.com>
2018-02-01 21:20:34 -0500

    Set library style to only use full namespace style for all source files

--- src/structs/queries/ovgfStructsQueriesVersion.cpp
+++ src/structs/queries/ovgfStructsQueriesVersion.cpp
@@ -1,13 +1,18 @@
 #include "structs\queries\ovgfStructsQueriesVersion.h"
 #include <vulkan\vulkan.hpp>
 
-namespace os = ovgf::structs;
+namespace ovgf {
+	namespace structs {
+		namespace queries {
 
-uint32_t os::queries::queryVersionNumber(Version ver) {
+			uint32_t queryVersionNumber(Version ver) {
+				if (ver.versionMajor == '1' && ver.versionMinor == '0') {
+					return VK_API_VERSION_1_0;
+				} else {
+					return -1; //Invalid version is just a validation problem
+				}
+			}
 
-	if (ver.versionMajor == '1' && ver.versionMinor == '0') {
-		return VK_API_VERSION_1_0;
-	} else {
-		return -1; //Invalid version is just a validation problem
+		}
 	}
 }
