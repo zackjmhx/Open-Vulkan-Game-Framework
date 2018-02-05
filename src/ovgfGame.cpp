4c283edec7abee06fc8dc8b0e5b3b930d4686105
zackjmhx <zack.jmh@gmail.com>
2018-02-05 11:10:59 -0500

    Game is now for merging user defined functionality with the instance
    No user functionality yet, so this just passes calls through to the instance for now

--- src/ovgfGame.cpp
+++ src/ovgfGame.cpp
@@ -1,46 +1,34 @@
 /**OVGF is meant, for now, to be a Vulkan based rapid prototyping tool to assist in game development.
 ***Once the framework is sufficient as such a tool, then development will shift focus to being suitable for release products*/
-#include "ovfgGame.h"
-#include <iostream>
-#include <chrono>
-#include <GLFW\glfw3.h>
 
-namespace sc = ::std::chrono;
-using sc::seconds;
+//This class is currently a little redundant to Instance. TODO: Add user function definition capability
+#include "ovfgGame.hpp"
+#include "objects\ovgfObjectsInstance.hpp"
 
 namespace ovgf {
 
-	Game::Game() : window() {
+	struct Game::GameImpl {
+	public:
+		GameImpl(std::string name, int wWidth, int wHeight);
+		void launch();
+
+	private:
+		objects::Instance instance; //Internally relies on GLFW for window abstraction - implements basic functionality
+	};
+
+	Game::Game(std::string name, int wWidth, int wHeight) : pGameImpl(spimpl::make_unique_impl<GameImpl>(name, wWidth, wHeight)) {
+	}
+
+	Game::GameImpl::GameImpl(std::string name, int wWidth, int wHeight) : instance(name, wWidth, wHeight) {
 	}
 
 
 	void Game::launch() {
-
-		if (window.getWindow(600, 400) == nullptr)
-			throw ::std::runtime_error("Failed to create OVGF window.");
-
-		mainLoop();
+		pGameImpl->launch();
 	}
 
-
-
-	void Game::mainLoop() {
-
-		auto times = sc::high_resolution_clock::now();
-		uint32_t frames = 0;
-
-		while (!glfwWindowShouldClose(window.getWindow())) {
-
-			glfwPollEvents();
-
-			frames++;
-
-			if ((sc::high_resolution_clock::now() - times) >= seconds{ 1 }) {
-				::std::cout << "Current FPS: " << frames << ::std::endl;
-				frames = 0;
-				times = ::std::chrono::high_resolution_clock::now();
-			}
-		}
-
+	void Game::GameImpl::launch() {
+		instance.launch();
 	}
+
 }
