8e100aed82f993d7d4084c98df330cf0d338998e
zackjmhx <zack.jmh@gmail.com>
2018-02-05 11:13:43 -0500

    Remove incorrect ''

--- src/structs/queries/ovgfStructsQueriesVersion.cpp
+++ src/structs/queries/ovgfStructsQueriesVersion.cpp
@@ -1,4 +1,4 @@
-#include "structs\queries\ovgfStructsQueriesVersion.h"
+#include "structs\queries\ovgfStructsQueriesVersion.hpp"
 #include <vulkan\vulkan.hpp>
 
 namespace ovgf {
@@ -6,10 +6,10 @@ namespace ovgf {
 		namespace queries {
 
 			uint32_t queryVersionNumber(Version ver) {
-				if (ver.versionMajor == '1' && ver.versionMinor == '0') {
+				if (ver.versionMajor == 1 && ver.versionMinor == 0) {
 					return VK_API_VERSION_1_0;
 				} else {
-					return -1; //Invalid version is just a validation problem
+					return 0; //Invalid version presented
 				}
 			}
 
